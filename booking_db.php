f5574d8459f9792a7786bf85f47c881ea0642061
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-01-29 21:37:23 +0530

    Update booking_db.php

--- booking_db.php
+++ booking_db.php
@@ -1,34 +1,30 @@
 <?php
-if(isset($_POST['submit']))
-{
-	$name=$_POST['name'];
-	$age=$_POST['age'];
-	$dob=$_POST['dob'];
-	$sex=$_POST['sex'];
-	$vaccine=$_POST['vaccine'];
-	$dose=$_POST['dose'];
-	$state=$_POST['state'];
-	$district=$_POST['district'];
+$dbconnect=mysqli_connect('localhost','avanthika','Abcd_1234','booking');
+
+$name=$_POST['name'];
+$age=$_POST['age'];
+$dob=$_POST['dob'];
+$sex=$_POST['sex'];
+$vaccine=$_POST['vaccine'];
+$dose=$_POST['dose'];
+$state=$_POST['state'];
+$district=$_POST['district'];
 	
+if (!$dbconnect)
+{
+	die("Database connection failed: ",mysqli_connect_error());
+}
 
-	$hostname="localhost";
-	$username="avanthika";
-	$password="Abcd_1234";
-	$db="booking";
 
-	$dbconnect=mysqli_connect($hostname,$username,$password,$db);
-	if (!$dbconnect)
+$query="INSERT INTO users_booked(name, age, dob, sex, vaccine, dose, state, district) VALUES('$name','$age','$dob','$sex','$vaccine','$dose','$state','$district')";
+if (!mysqli_query($dbconnect, $query))
 	{
-		die("Database connection failed: ",mysqli_connect_error());
-	}
-
-
-	$query="INSERT INTO users_booked(name,age,dob,sex,vaccine,dose,state,dist) VALUES('$name','$age','$dob','$sex','$vaccine','$dose','$state','$dist')";
-	if (!mysqli_query($dbconnect, $query)) {
-        die('An error occurred. Booking not Successful');
-    } else {
-      echo "The Vaccine has been booked successfully";
-    }
+    die('An error occurred. Booking not Successful');
+	} 
+  else 
+  {
+    echo "The Vaccine has been booked successfully";
+  }
 
     mysqli_close($dbconnect);
 
