0afb7fe58c4defa9d82a3eb332934971b63ece2e
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-01-30 00:26:01 +0530

    Create book.php

---
+++ book.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Successfully booked</title>
+</head>
+<body>
+	<center><b><i><h1>Your Vaccine has been successfully booked!<h1><br>
+	Thankyou!</i></b><br><br>
+	<button onclick="document.location='logout.php'">Logout</button>
+</center>
+</body>
+</html>
