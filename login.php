a4253b208ebbd86b401f2430b482b6d8dc042bf9
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-01-31 19:47:52 +0530

    Update login.php

--- login.php
+++ login.php
@@ -12,17 +12,20 @@
 	<div class="header">
 		<h2>Login</h2>
 	</div>
-	<form method="post" action="login.php">
+	<form method="post" action="cookies.php">
 		
 		<div class="input-group">
 			<label>Mobile Number</label>
-			<input type="number" name="mobileno">
+			<input type="number" name="mobileno" value="<?php if(isset($_COOKIE["mobileno"])) { echo $_COOKIE["mobileno"]; } ?>">
 		</div>
 		<div class="input-group">
 			<label>Password</label>
-			<input type="password" name="password">
+			<input type="password" name="password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
 		</div>
 		<div class="input-group">
+			<p>
+				<input type="checkbox" name="remember" /> Remember me
+			</p>
 			<button type="submit" class="btn" name="login_user">Login</button>
 		</div>
 		<p>
