298e992a978ee44eb288d735bbc5f5b1d599ad00
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-01-31 18:17:48 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,15 +1,42 @@
 <?php
 require('db.php');
 
-$name_of_center=$_GET['name_of_center'];
-$slot=$_GET['slot'];
-$sql="INSERT INTO vax_centers(name_of_center,slot) VALUES('$name_of_center','$slot')";
-if(mysqli_query($link,$sql)){
-    echo "new recrd has been added succesfully!";
+$status = "";
+if(isset($_POST['new']) && $_POST['new']==1){
+    $name_of_center =$_REQUEST['name_of_center'];
+    $slot = $_REQUEST['slot'];
+    $user = $_SESSION["login_user"];
+    $ins_query="insert into vac_center
+    (name_of_center,slot,user)values
+    ('$name_of_center','$slot','$user')";
+    mysqli_query($link,$ins_query)
+    or die(mysql_error());
+    $status = "New Record Inserted Successfully.
+    </br></br><a href='view.php'>View Inserted Record</a>";
 }
-else
-{
-    echo "Error:". $sql.":-".mysqli_error($link);
-}
-mysqli_close($link);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert New Record</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p><a href="dashboard.php">Dashboard</a> 
+| <a href="view.php">View Records</a> 
+| <a href="logout.php">Logout</a></p>
+<div>
+<h1>Insert New Record</h1>
+<form name="form" method="post" action=""> 
+<input type="hidden" name="new" value="1" />
+<p><input type="text" name="name_of_center" placeholder="Enter name of center" required /></p>
+<p><input type="text" name="slot" placeholder="Enter slots" required /></p>
+<p><input name="submit" type="submit" value="Submit" /></p>
+</form>
+<p style="color:#FF0000;"><?php echo $status; ?></p>
+</div>
+</div>
+</body>
+</html>
