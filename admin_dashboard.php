97c48e5dbdd2f1913942ee0351c8c402d9ac3d07
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-01-31 18:19:53 +0530

    Create admin_dashboard.php

---
+++ admin_dashboard.php
@@ -0,0 +1,22 @@
+<?php
+require('db.php');
+include('config.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Admin</title>
+
+<body>
+<div class="form"><center>
+<p><h1>Welcome to Admin Panel<h1></p>
+<p><a href="admin.php">Home</a><p>
+<p><a href="insert.php">Insert New record</a></p>
+<p><a href="view.php">View records</a></p>
+<p><a href="logout.php">Logout</a></p>
+<center>
+</div>
+</body>
+</html>
