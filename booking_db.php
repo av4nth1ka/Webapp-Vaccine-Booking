0e62d7be9fc842206554a4bf08cff0ec9a17a7ff
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-03-11 15:46:05 +0530

    Update booking_db.php

--- booking_db.php
+++ booking_db.php
@@ -1,32 +1,28 @@
 <?php
+
 $dbconnect=mysqli_connect('localhost','avanthika','Abcd_1234','booking');
 
-$name=$_POST['name'];
-$age=$_POST['age'];
-$dob=$_POST['dob'];
-$sex=$_POST['sex'];
-$vaccine=$_POST['vaccine'];
-$dose=$_POST['dose'];
-$state=$_POST['state'];
-$district=$_POST['district'];
-	
-if (!$dbconnect)
+if(isset($_POST['submit']))
 {
-	die("Database connection failed: ",mysqli_connect_error());
+  $name=$_POST['name'];
+  $age=$_POST['age'];
+  $dob=$_POST['dob'];
+  $sex=$_POST['sex'];
+  $vaccine=$_POST['vaccine'];
+  $dose=$_POST['dose'];
+  $state=$_POST['state'];
+  $district=$_POST['district'];
+
+}
+
+$query="INSERT INTO `users_booked`(`name`,`age`,`dob`,`sex`,`vaccine`,`dose`,`state`,`district`) VALUES('$name','$age','$dob','$sex','$vaccine','$dose','$state','$district')";
+$rs=mysqli_query($dbconnect,$query);
+if($rs)
+{
+  echo "Record entered successfully";
+  header('location:upload.php');
 }
 
 
-$query="INSERT INTO users_booked(name, age, dob, sex, vaccine, dose, state, district) VALUES('$name','$age','$dob','$sex','$vaccine','$dose','$state','$district')";
-if (!mysqli_query($dbconnect, $query))
-	{
-    die('An error occurred. Booking not Successful');
-	} 
-  else 
-  {
-    echo "The Vaccine has been booked successfully";
-  }
-
-    mysqli_close($dbconnect);
-
-}
 ?>
+
