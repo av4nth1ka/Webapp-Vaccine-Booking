5101f1a30478eb8fdfd4441c6acd5138c21ae6fa
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-03-11 15:33:05 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,35 +3,33 @@
 <html>
 <head>
 	<title>Login page</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	
 </head>
 <body>
-
-
-
+	<center>
 	<div class="header">
-		<h2>Login</h2>
+		<h1>Login</h1>
 	</div>
 	<form method="post" action="cookies.php">
 		
-		<div class="input-group">
+		<div>
 			<label>Mobile Number</label>
-			<input type="number" name="mobileno" value="<?php if(isset($_COOKIE["mobileno"])) { echo $_COOKIE["mobileno"]; } ?>">
-		</div>
-		<div class="input-group">
+			<input type="number" name="mobileno" value="<?php if(isset($_COOKIE["mobileno"])) { echo $_COOKIE["mobileno"]; } ?>"> 
+		</div><br><br>
+		<div>
 			<label>Password</label>
-			<input type="password" name="password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
-		</div>
-		<div class="input-group">
+			<input type="password" name="password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>"> 
+		</div><br><br>
+		<div>
 			<p>
 				<input type="checkbox" name="remember" /> Remember me
-			</p>
+			</p><br>
 			<button type="submit" class="btn" name="login_user">Login</button>
-		</div>
+		</div><br>
 		<p>
 			Not yet registered?<a href="register.php">Register</a>
 		</p>
 	</form>
-
+</center>
 </body>
 </html>
