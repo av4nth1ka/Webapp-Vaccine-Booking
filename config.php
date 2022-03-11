2de256f44c3609608eb61d70768e1c0ec3e4283b
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2022-03-11 15:38:51 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,73 +1,65 @@
 <?php
 session_start();
-//initialised the variables
-$username="";
-$mobileno="";
-$errors=array();
-
 $db=mysqli_connect('localhost','avanthika','Abcd_1234','register');
-//REGISTER USE
+//REGISTER USER
+
 if(isset($_POST['reg_user']))
 {
-	//to receive values from the user
-	$username=mysqli_real_escape_string($db,$_POST['username']);
-	$mobileno=mysqli_real_escape_string($db,$_POST['mobileno']);
-	$password=mysqli_real_escape_string($db,$_POST['password']);
-
-	//to ensure the form is correctly filled 
-	//by using array_push()
-	if(empty($username)) { array_push($errors, "Username is required");}
-	if(empty($mobileno)) {array_push($errors, "Mobile number is required");}
-	if(empty($password)) {array_push($errors, "Password is required");}
-
-	//to ensure a user doesnt exist with the given credentials
-	$user_check_query="SELECT * FROM user WHERE username='$username' OR mobileno='$mobileno' LIMIT 1";
-	$result=mysqli_query($db,$user_check_query);
-	$user=mysqli_fetch_assoc($result);
-
-	if($user) { //if user already exists
-		if($user['mobileno']==='$mobileno'){
-			array_push($errors, "Mobile number already exists");
-
-		}
-	}
-
-	//if there are no errors, register the user
-	if(count($errors)==0){
-		$password=md5($password); //encrypts the password b4 saving to database
-
-		$query="INSERT INTO user(username,mobileno,password)VALUES('$username','$mobileno','$password')";
-		mysqli_query($db, $query);
-		$_SESSION['username']=$username;    //implementing sessions
+	//Getting Post Values
+	$username=$_POST['username'];	
+	$mobileno=$_POST['mobileno'];	
+	$password=$_POST['password'];	
+	//Checking mobile number id exist for not
+	$result ="SELECT count(*) FROM user WHERE mobileno=?";
+	$stmt = $db->prepare($result);
+	$stmt->bind_param('i',$mobileno);
+	$stmt->execute();
+	$stmt->bind_result($count);
+	$stmt->fetch();
+	$stmt->close();
+	//if mobileno already exist
+	if($count>0)
+	{
+		echo "<script>alert('mobile number already associated with another account. Please try with diffrent number.');</script>";
+	} 
+	// If mobile number not exist
+	else 
+	{
+		$sql="INSERT into user(username,mobileno,password)VALUES(?,?,?)";
+		$stmti = $db->prepare($sql);
+		$stmti->bind_param('sis',$username,$mobileno,$password);
+		$stmti->execute();
+		$stmti->close();
+        $_SESSION['username']=$username;    //implementing sessions
 		$_SESSION['success']="You are now logged in";
 		header('location:contents.php');
 	}
+	
 }
 
-//Login user
-if(isset($_POST['login_user'])) {
-	$mobileno=mysqli_real_escape_string($db,$_POST['mobileno']);
-	$password=mysqli_real_escape_string($db,$_POST['password']);
 
-	if(empty($mobileno)) {
-		array_push($errors,"Mobile Number is required");
-	}
-	if(empty($password)){
-		array_push($errors,"Password is required");
-	}
-	if(count($errors)==0){
-		$password=md5($password);
-		$query="SELECT * FROM user WHERE mobileno='$mobileno' AND password='$password' ";
-		$results=mysqli_query($db,$query);
-		if(mysqli_num_rows($results)==1)
-		{
-			$_SESSION['mobileno']=$mobileno;
-			$_SESSION['success']="You are now logged in";
-			header('Location:contents.php');
-		}else{
-			array_push($errors,"Wrong mobile number or password");
-		}
-
-	}
+//login user
+if(isset($_POST['login_user']))
+{
+	//Getting Post Values
+	$mobileno=$_POST['mobileno'];	
+	$password=$_POST['password'];	
+	$stmt = $db->prepare( "SELECT * FROM user WHERE mobileno='$mobileno' AND password='$password' ");
+	$stmt->bind_param('is',$mobileno,$password);
+    $stmt->execute();
+    $stmt->bind_result($mobileno,$id);
+    $rs= $stmt->fetch ();
+    $stmt->close();
+    if (!$rs) 
+    {
+  		echo "<script>alert('Invalid Details. Please try again.')</script>";
+    } 
+    else 
+    {
+    	$_SESSION['mobileno']=$mobileno;
+		$_SESSION['success']="You are now logged in";
+     	header('location:contents.php');
+    }
 }
+ 
 ?>
